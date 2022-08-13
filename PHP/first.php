9e0455a6183c9a44cab466324f0d1657a16a957c
kkaur1998 <kamaljeetkaur.malik@gmail.com>
2022-08-13 11:17:15 +0530

    PHP

---
+++ PHP/first.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP PRogram</title>
+    </head>
+    <body>
+        <h1>First PHP page</h1>
+        <?php
+        echo "Hello KK"
+        ?>
+    </body>
+</html>
+
